a4b36abbaac41534d9f4e76813bf23e19c9a5fef
Christophe Bonello <christophe.bonello@gmail.com>
2013-09-08 22:30:08 +0530

    first commit

---
+++ samples/demo/app/controllers/app.go
@@ -0,0 +1,28 @@
+package controllers
+
+import (
+    "github.com/robfig/revel"
+    "csrf-nosurf/app/routes"
+    "fmt"
+)
+
+type App struct {
+	*revel.Controller
+}
+
+func (c App) Index() revel.Result {
+	return c.Render()
+}
+
+func (c App) Hello(name string) revel.Result {
+	return c.Render(name)
+}
+
+func (c App) Logout(name string) revel.Result {
+	fmt.Printf("Deleting session keys...\n")
+	for k := range c.Session {
+		fmt.Printf("Deleting Session[%s]: '%s'\n", k, c.Session[k])
+		delete(c.Session, k)
+	}
+	return c.Redirect(routes.App.Index())
+}
